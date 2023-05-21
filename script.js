acf84d354ed191e044f664e723b16b000cfa92d6
boim <ibrahimmuh26@gmail.com>
2023-05-21 20:41:07 +0700

    calcu js fix

---
+++ script.js
@@ -0,0 +1,17 @@
+function plusValue(value) {
+  document.getElementById("result").value += value;
+}
+
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+
+function calculate() {
+  var result = eval(document.getElementById("result").value);
+  document.getElementById("result").value = result;
+}
+
+function minusValue() {
+  var value = document.getElementById("result").value;
+  document.getElementById("result").value = value.slice(0, -1);
+}
